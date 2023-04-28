494348226dd1ca93b888b366d0344bdb0f7a9654
jungwoo343 <jungwoo343@naver.com>
2023-04-28 11:27:24 +0900

    관리자 추가

--- SemiProject/src/com/semi/member/model/service/MemberService.java
+++ SemiProject/src/com/semi/member/model/service/MemberService.java
@@ -30,6 +30,8 @@ public class MemberService {
 		int idCheck = new MemberDao().checkId(conn,memberId);
 		
 		JDBCTemplate.close(conn);
+				
+		
 		
 		return idCheck;
 	}
@@ -54,7 +56,10 @@ public class MemberService {
 	public String selectId(Member m) {
 		Connection conn = JDBCTemplate.getConnection();
 		
+		
 		String srcIdM = new MemberDao().selectId(conn,m);
+		
+		
 		
 		JDBCTemplate.close(conn);
 		return srcIdM;
@@ -64,6 +69,10 @@ public class MemberService {
 		Connection conn = JDBCTemplate.getConnection();
 		
 		Member pwdM = new MemberDao().searchPwd(conn,srcPwdM);
+		
+		 
+		
+		
 		
 		JDBCTemplate.close(conn);
 		return pwdM;
@@ -75,103 +84,106 @@ public class MemberService {
 		ArrayList<Payment> list = new MemberDao().selectShoppingList(conn);
 		
 		JDBCTemplate.close(conn);
+
 		
 		return list;
 	}
 	
 	//총 회원 수
-		public int selMemberAdminCount() {
-			
-			Connection conn = JDBCTemplate.getConnection();
-			
-			int listCount = new MemberDao().selMemberAdminCount(conn);
-			
-			JDBCTemplate.close(conn);
-			
-			return listCount;
-		}
+	public int selMemberAdminCount() {
 		
-		//상세 검색 회원 수
-		public int searchAdminCount(int num, String search) {
-			
-			Connection conn = JDBCTemplate.getConnection();
-			
-			int listCount = new MemberDao().searchAdminCount(conn,num,search);
-			
-			JDBCTemplate.close(conn);
-			
-			return listCount;
-		}
+		Connection conn = JDBCTemplate.getConnection();
 		
-		//회원 관리 조회
-		public ArrayList<Member> selectMemberAdmin(PageInfo pi) {
-			
-			Connection conn = JDBCTemplate.getConnection();
-			
-			ArrayList<Member> list = new MemberDao().selectMemberAdmin(conn,pi);
-			
-			JDBCTemplate.close(conn);
-			
-			return list;
-		}
+		int listCount = new MemberDao().selMemberAdminCount(conn);
 		
-		//회원 상세조회
-		public Member detailMemberAdmin(int umno) {
-			
-			Connection conn = JDBCTemplate.getConnection();
-			
-			Member m = new MemberDao().detailMemberAdmin(conn,umno);
-			
-			JDBCTemplate.close(conn);
-					
-			return m;
-		}
+		JDBCTemplate.close(conn);
 		
-		//회원 상세검색
-		public ArrayList<Member> searchIdAdmin(String search, PageInfo pi, int num) {
-			
-			Connection conn = JDBCTemplate.getConnection();
-			
-			ArrayList<Member> list = new MemberDao().searchIdAdmin(conn,search,pi,num);
-			
-			JDBCTemplate.close(conn);
-			
-			return list;
-		}
+		return listCount;
+	}
+	
+	//상세 검색 회원 수
+	public int searchAdminCount(int num, String search) {
+		
+		Connection conn = JDBCTemplate.getConnection();
+		
+		int listCount = new MemberDao().searchAdminCount(conn,num,search);
+		
+		JDBCTemplate.close(conn);
+		
+		return listCount;
+	}
+	
+	//회원 관리 조회
+	public ArrayList<Member> selectMemberAdmin(PageInfo pi) {
+		
+		Connection conn = JDBCTemplate.getConnection();
+		
+		ArrayList<Member> list = new MemberDao().selectMemberAdmin(conn,pi);
+		
+		JDBCTemplate.close(conn);
+		
+		return list;
+	}
+	
+	//회원 상세조회
+	public Member detailMemberAdmin(int umno) {
+		
+		Connection conn = JDBCTemplate.getConnection();
+		
+		Member m = new MemberDao().detailMemberAdmin(conn,umno);
+		
+		JDBCTemplate.close(conn);
+				
+		return m;
+	}
+	
+	//회원 상세검색
+	public ArrayList<Member> searchIdAdmin(String search, PageInfo pi, int num) {
+		
+		Connection conn = JDBCTemplate.getConnection();
+		
+		ArrayList<Member> list = new MemberDao().searchIdAdmin(conn,search,pi,num);
+		
+		JDBCTemplate.close(conn);
+		
+		return list;
+	}
 
-		//회원 수정
-		public int updateMemberAdmin(Member m) {
-			
-			Connection conn = JDBCTemplate.getConnection();
-			
-			int result = new MemberDao().updateMemberAdmin(conn,m);
-			
-			if(result > 0) {
-				JDBCTemplate.commit(conn);
-			}else {
-				JDBCTemplate.rollback(conn);
-			}
-			
-			JDBCTemplate.close(conn);
-			
-			return result;
+	//회원 수정
+	public int updateMemberAdmin(Member m) {
+		
+		Connection conn = JDBCTemplate.getConnection();
+		
+		int result = new MemberDao().updateMemberAdmin(conn,m);
+		
+		if(result > 0) {
+			JDBCTemplate.commit(conn);
+		}else {
+			JDBCTemplate.rollback(conn);
 		}
+		
+		JDBCTemplate.close(conn);
+		
+		return result;
+	}
 
-		//회원 삭제
-		public int deleteMemberAdmin(int delMno) {
-			
-			Connection conn = JDBCTemplate.getConnection();
-			
-			int result = new MemberDao().deleteMemberAdmin(conn,delMno);
-			
-			if(result > 0) {
-				JDBCTemplate.commit(conn);
-			}else {
-				JDBCTemplate.rollback(conn);
-			}
-			
-			JDBCTemplate.close(conn);
-			
-			return result;
+	//회원 삭제
+	public int deleteMemberAdmin(int delMno) {
+		
+		Connection conn = JDBCTemplate.getConnection();
+		
+		int result = new MemberDao().deleteMemberAdmin(conn,delMno);
+		
+		if(result > 0) {
+			JDBCTemplate.commit(conn);
+		}else {
+			JDBCTemplate.rollback(conn);
 		}
+		
+		JDBCTemplate.close(conn);
+		
+		return result;
+	}
+
+
 }
